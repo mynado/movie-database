6c0a3fdb8bbf8bf9cf6536f19b810cc1d12e1374
MyNa Do <myna.do@mahoyo.com>
2020-05-28 10:45:20 +0200

    Added movie routes and movie controller

---
+++ controllers/movie_controller.js
@@ -0,0 +1,66 @@
+/**
+ * Movie Controller
+ */
+
+/**
+ * Get all movies
+ * GET /
+ */
+const index = async (req, res) => {
+	res.status(405).send({
+		status: 'fail',
+		message: 'Method not implemented',
+	});
+}
+
+/**
+ * Get a specific movie
+ * GET /:movieId
+ */
+const show = async (req, res) => {
+	res.status(405).send({
+		status: 'fail',
+		message: 'Method not implemented',
+	});
+}
+
+/**
+ * Create a new movie
+ * POST /
+ */
+const store = async (req, res) => {
+	res.status(405).send({
+		status: 'fail',
+		message: 'Method not implemented',
+	});
+}
+
+/**
+ * Update a movie
+ * PUT /:movieId
+ */
+const update = async (req, res) => {
+	res.status(405).send({
+		status: 'fail',
+		message: 'Method not implemented',
+	});
+}
+
+/**
+ * Delete a movie
+ * DELETE /:movieId
+ */
+const destroy = async (req, res) => {
+	res.status(405).send({
+		status: 'fail',
+		message: 'Method not implemented',
+	});
+}
+
+module.exports = {
+	index,
+	show,
+	store,
+	update,
+	destroy,
+}
