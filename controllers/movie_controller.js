1ebce63ec3d7fdbcc8eeb1e7d6e9ec52f3a1a3f0
MyNa Do <myna.do@mahoyo.com>
2020-05-28 12:28:43 +0200

    create a new movie and get all movies

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -9,10 +9,24 @@ const models = require('../models');
  * GET /
  */
 const index = async (req, res) => {
-	res.status(405).send({
-		status: 'fail',
-		message: 'Method not implemented',
-	});
+	try {
+		//const movie = await new models.Movie(req.body).save();
+		const movies = await models.Movie.find();
+
+		res.status(200).send({
+			status: 'success',
+			data: {
+				movies,
+			},
+		});
+
+	} catch (error) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown when trying to get all movies.'
+		});
+		throw error;
+	}
 }
 
 /**
@@ -31,6 +45,24 @@ const show = async (req, res) => {
  * POST /
  */
 const store = async (req, res) => {
+	console.log('incoming new movie data', req.body);
+	try {
+		const movie = await new models.Movie(req.body).save();
+
+		res.status(201).send({
+			status: 'success',
+			data: {
+				movie,
+			},
+		});
+
+	} catch (error) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown when trying to create new movie.'
+		});
+		throw error;
+	}
 	res.status(405).send({
 		status: 'fail',
 		message: 'Method not implemented',
